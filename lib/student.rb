25a3d302c873f0ae339eaf2d94040e6dbb4a3578
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-20 02:38:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
 
       else
         DB[:conn].execute("INSERT INTO students(name, grade) VALUES (?, ?)", self.name, self.grade)
-        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       end
     end
 
